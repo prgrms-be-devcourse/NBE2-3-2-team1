629dc0c483849d6e531984309d03fb47afefcb7e
JinWoong <slsl03264@naver.com>
2024-12-19 14:46:48 +0900

    feat: card 기능 구현 각각 어제와 오늘의 총 users, comments, hits를 나누어 성장률을 카드에 각각 담을 인자를 구현

--- src/main/java/org/programmers/cocktail/repository/authorities/AuthoritiesRepository.java
+++ src/main/java/org/programmers/cocktail/repository/authorities/AuthoritiesRepository.java
@@ -2,8 +2,9 @@ package org.programmers.cocktail.repository.authorities;
 
 import org.programmers.cocktail.entity.Authorities;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface AuthoritiesRepository extends JpaRepository<Authorities, Long> {
+public interface AuthoritiesRepository extends JpaRepository<Authorities, Long>, AuthoritiesRepositoryCustom {
 
     int countByRole(String role);
 }
