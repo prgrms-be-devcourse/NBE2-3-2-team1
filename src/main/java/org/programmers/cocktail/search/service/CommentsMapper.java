c4f0f5413bc21b16634de44f9cc98bb26434345d
JaeHyunPyun <pyunjaehyun@gmail.com>
2024-12-20 16:07:11 +0900

    refactor: 새로 생성한 mapper 클래스 등록
    이전 커밋에서 누락된 mapper 클래스를 추가

---
+++ src/main/java/org/programmers/cocktail/search/service/CommentsMapper.java
@@ -0,0 +1,54 @@
+package org.programmers.cocktail.search.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
+import org.programmers.cocktail.entity.CocktailLists;
+import org.programmers.cocktail.entity.Comments;
+import org.programmers.cocktail.search.dto.CocktailListsTO;
+import org.programmers.cocktail.search.dto.CommentsTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentsMapper {
+    private ModelMapper modelMapper;
+
+    @Autowired
+    public CommentsMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+
+        // Comments -> CommentsTO 변환 매핑
+        modelMapper.addMappings(new PropertyMap<Comments, CommentsTO>() {
+            @Override
+            protected void configure() {
+                map().setUserId(source.getUsers().getId());
+                map().setCocktailId(source.getCocktails().getId());
+            }
+        });
+
+        // CommentsTO -> Comments 변환 매핑
+        modelMapper.addMappings(new PropertyMap<CommentsTO, Comments>() {
+            @Override
+            protected void configure() {
+                map().getUsers().setId(source.getUserId());
+                map().getCocktails().setId(source.getCocktailId());
+            }
+        });
+    }
+
+    public List<CommentsTO> convertToCommentsTOList(List<Comments> commentsList) {
+        List<CommentsTO> commentsTOList = commentsList.stream()
+            .map(comments -> modelMapper.map(comments, CommentsTO.class))
+            .collect(Collectors.toList());
+        return commentsTOList;
+    }
+
+    public List<Comments> convertToCommentsList(List<CommentsTO> commentsTOList) {
+        List<Comments> commentsList = commentsTOList.stream()
+            .map(commentsTO -> modelMapper.map(commentsTO, Comments.class))
+            .collect(Collectors.toList());
+        return commentsList;
+    }
+}
