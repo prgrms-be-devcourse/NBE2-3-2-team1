c3d7f9585f49b119a876405342ab22368eebc3a5
jeongeunlee <dearellen@yonsei.ac.kr>
2024-12-21 20:54:47 +0900

    feat: 회원 정보 수정

--- src/main/java/org/programmers/cocktail/login/controller/LoginController.java
+++ src/main/java/org/programmers/cocktail/login/controller/LoginController.java
@@ -1,7 +1,5 @@
 package org.programmers.cocktail.login.controller;
 
-import static java.lang.System.out;
-
 import jakarta.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -91,8 +89,8 @@ public class LoginController {
         HttpSession session,
         Model model
     ) {
-        out.println("email: " + email);
-        out.println("password: " + password);
+        System.out.println("email: " + email);
+        System.out.println("password: " + password);
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
         // 로그인창에서 입력받은 이메일과 패스워드
@@ -106,13 +104,13 @@ public class LoginController {
 
         int flag = 2;
 
-        out.println("users.getEmail(): " + users.getEmail());
-        out.println("users.getPassword(): " + users.getPassword());
+        System.out.println("users.getEmail(): " + users.getEmail());
+        System.out.println("users.getPassword(): " + users.getPassword());
 
         if ( saveEmail.equals(users.getEmail()) && encoder.matches(savePassword, users.getPassword())) {
             flag = 0;
 
-            out.println("flag0: " + flag);
+            System.out.println("flag0: " + flag);
 
             // session 부여
             // 이후 세션에 저장된 semail 값은 사용자가 페이지를 이동하더라도 유지되며, 다른 요청에서 접근할 수 있음
@@ -122,7 +120,7 @@ public class LoginController {
 
         } else {
             flag = 1;
-            out.println("flag1: " + flag);
+            System.out.println("flag1: " + flag);
             response.put("message", "이메일 또는 비밀번호가 일치하지 않습니다.");
         }
 
@@ -137,7 +135,7 @@ public class LoginController {
     @PostMapping("/logout_ok")
     public String logout_ok(HttpSession session) {
 
-        out.println("session.getAttributeBefore: " + session.getAttribute("semail"));
+        System.out.println("session.getAttributeBefore: " + session.getAttribute("semail"));
 
         session.invalidate();
 
@@ -164,52 +162,6 @@ public class LoginController {
 
 
 
-    // 이메일, 비밀번호, 이름, 생년월일(아마) select하기 (mypage에 띄워놓기: 프론트)
-    @PostMapping("/login/mypage")
-    public UserRegisterDto mypage(
-        @RequestParam("email") String email
-    ) {
-
-        // DB에서 이메일, 패스워드 가져오기
-        Users users = loginService.findByEmail( email );
-
-        // dto에 users값 넣기
-        UserRegisterDto to = new UserRegisterDto();
-        to.setId( users.getId() );
-        to.setEmail( users.getEmail() );
-        to.setPassword( users.getPassword() );
-        to.setName( users.getName() );
-
-
-        return to;
-    }
-
-
-//    @PostMapping("/logout_ok")
-//    public String logout_ok(HttpSession session) {
-//        // 세션 아이디 파기 후 재생성
-//        // logout후 login_complete으로 이동 시 세션 아이디가 없어져 flag = 1이 된다.
-//        session.invalidate();
-//
-//        return "logout_ok";
-//    }
-
-
-
-//    @GetMapping("/mypage")
-//    public String showMyPage(
-//        HttpSession session,
-//        RedirectAttributes redirectAttributes) {
-//
-//
-//        if(session.getAttribute("semail") != null) {
-//            return "user/mypage"; // "mypage.html" 템플릿을 반환
-//        } else {
-//            redirectAttributes.addFlashAttribute("message", "로그인해야 합니다.");
-//            return "redirect:/login";
-//        }
-//    }
-
     @GetMapping("/mypage")
     public String showMyPage(
         HttpSession session,
@@ -217,6 +169,24 @@ public class LoginController {
         RedirectAttributes redirectAttributes) {
 
         String email = (String) session.getAttribute("semail");
+        List<CocktailLists> cocktailLists = cocktailListsRepository.findAll();
+        List<Cocktails> cocktails = cocktailsRepository.findAll();
+
+
+        List<CocktailsDto> cocktailsDtos = new ArrayList<>();
+
+        for ( Cocktails cocktail : cocktails ) {
+            CocktailsDto cocktailsTo = new CocktailsDto();
+            cocktailsTo.setId(cocktail.getId());
+            cocktailsTo.setName(cocktail.getName());
+            cocktailsTo.setImage_url(cocktail.getImage_url());
+            cocktailsDtos.add(cocktailsTo);
+        }
+
+        // 칵테일 정보 잘 가져오는지 확인
+        for (CocktailsDto c : cocktailsDtos) {
+            System.out.println("CocktailsDto: " + c);
+        }
 
         if( email != null ) {
             // 세션에서 이메일을 통해 DB에서 유저 정보를 가져오기
@@ -232,12 +202,142 @@ public class LoginController {
             // 모델에 유저 정보를 담아서 뷰로 전달
             model.addAttribute("to", to);
 
+            // user, cocktails, cocktaillists 조인해서 user_id타고
+            // cocktaillists에 저장된 cocktail_id를 타서 cocktail에 가서 칵테일 정보 가져오기
+            List<CocktailsDto> ct = new ArrayList<>();
+
+            for (CocktailLists cl : cocktailLists) {
+                if ( users.getId().equals( cl.getUsers().getId() ) ) {
+                    for (CocktailsDto c : cocktailsDtos) {
+                        if (cl.getCocktails().getId().equals(c.getId())) {
+                            ct.add( c );
+                        }
+                    }
+                }
+            }
+
+            model.addAttribute("ct", ct);
+
             return "user/mypage";
         } else {
             redirectAttributes.addFlashAttribute("message", "로그인해야 합니다.");
             return "redirect:/login";
         }
     }
+
+    // 이메일은 변경 X
+    // 이메일로 유저의 Id를 가져와 비밀번호와 이름을 수정
+    @PostMapping("/modify_ok")
+    public ResponseEntity<Map<String, Object>> modify_ok(
+        @RequestParam("email") String email,
+        @RequestParam("name") String name,
+        @RequestParam("password") String password
+    ) {
+        System.out.println("modify_ok 호출");
+        Map<String, Object> response = new HashMap<>();
+
+        int flag = 2;
+
+        Users users = loginService.findByEmail( email );
+        System.out.println("modify_ok: " + users.getEmail());
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encodedPassword = encoder.encode(password);
+
+        System.out.println("users.getId(): " + users.getId());
+        System.out.println("name : " + name );
+
+        int result = loginService.updateUser( name, encodedPassword, users.getId());
+        System.out.println("result: " + result );
+
+        if (result == 0) {
+            flag = 0;
+            response.put("message", "계정 정보 수정 성공");
+        } else {
+            flag = 1;
+            response.put("message", "계정 정보 수정 실패\n로그인 페이지로 돌아갑니다.");
+        }
+
+
+        response.put("flag", flag);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/withdrawal_ok")
+    public ResponseEntity<Map<String, Object>> userWithdrawal(
+        @RequestParam("email") String email,
+        @RequestParam("password") String password
+    ) {
+        System.out.println("withdrawal_ok 호출");
+        Map<String, Object> response = new HashMap<>();
+
+        int flag = 2;
+
+        Users users = loginService.selectByEmailandPassword( email, password );
+        // System.out.println("users.getEmail(): " + users.getEmail()); // 잘 나옴
+
+        int result = 0;
+        if(email.equals(users.getEmail()) && password.equals(users.getPassword())) {
+            result = loginService.deleteUser( users.getId() );
+        }
+
+        if ( result == 0 ) {
+            flag = 1;
+            response.put("message", "탈퇴 실패");
+        } else {
+            flag = 0;
+            response.put("message", "탈퇴 성공");
+        }
+
+        response.put("flag", flag);
+
+        return ResponseEntity.ok(response);
+    }
+
+
+//    // Mypage에서 내가 찜한 칵테일 보여주기
+//    @PostMapping("/login/mypageUsersFavoriteCocktail")
+//    public List<CocktailsDto> mypageUsersFavoriteCocktail(
+//        @RequestParam("email") String email
+//    ) {
+//
+//        Users users = loginService.findByEmail( email );
+//        List<CocktailLists> cocktailLists = cocktailListsRepository.findAll();
+//        List<Cocktails> cocktails = cocktailsRepository.findAll();
+//
+//
+//        List<CocktailsDto> cocktailsDtos = new ArrayList<>();
+//
+//        for ( Cocktails cocktail : cocktails ) {
+//            CocktailsDto cocktailsTo = new CocktailsDto();
+//            cocktailsTo.setId(cocktail.getId());
+//            cocktailsTo.setName(cocktail.getName());
+//            cocktailsTo.setImage_url(cocktail.getImage_url());
+//            cocktailsDtos.add(cocktailsTo);
+//        }
+//
+//        // 칵테일 정보 잘 가져오는지 확인
+//        for (CocktailsDto c : cocktailsDtos) {
+//            out.println("CocktailsDto: " + c);
+//        }
+//
+//        List<CocktailsDto> ct = new ArrayList<>();
+//
+//        // user, cocktails, cocktaillists 조인해서 user_id타고
+//        // cocktaillists에 저장된 cocktail_id를 타서 cocktail에 가서 칵테일 정보 가져오기
+//        for (CocktailLists cl : cocktailLists) {
+//            if ( users.getId().equals( cl.getUsers().getId() ) ) {
+//                for (CocktailsDto c : cocktailsDtos) {
+//                    if (cl.getCocktails().getId().equals(c.getId())) {
+//                        ct.add( c );
+//                    }
+//                }
+//            }
+//        }
+//
+//        return ct;
+//    }
 
     // 이메일, 비밀번호, 이름, 생년월일(아마) select하기 (mypage에 띄워놓기: 프론트)
 //    @PostMapping("/login/mypageUserInformation")
@@ -260,114 +360,7 @@ public class LoginController {
 //
 //    }
 
-    // Mypage에서 내가 찜한 칵테일 보여주기
-    @PostMapping("/login/mypageUsersFavoriteCocktail")
-    public List<CocktailsDto> mypageUsersFavoriteCocktail(
-        @RequestParam("email") String email
-    ) {
 
-        Users users = loginService.findByEmail( email );
-        List<CocktailLists> cocktailLists = cocktailListsRepository.findAll();
-        List<Cocktails> cocktails = cocktailsRepository.findAll();
-
-
-        List<CocktailsDto> cocktailsDtos = new ArrayList<>();
-
-        for ( Cocktails cocktail : cocktails ) {
-            CocktailsDto cocktailsTo = new CocktailsDto();
-            cocktailsTo.setId(cocktail.getId());
-            cocktailsTo.setName(cocktail.getName());
-            cocktailsTo.setImage_url(cocktail.getImage_url());
-            cocktailsDtos.add(cocktailsTo);
-        }
-
-        // 칵테일 정보 잘 가져오는지 확인
-        for (CocktailsDto c : cocktailsDtos) {
-            out.println("CocktailsDto: " + c);
-        }
-
-        List<CocktailsDto> ct = new ArrayList<>();
-
-        // user, cocktails, cocktaillists 조인해서 user_id타고
-        // cocktaillists에 저장된 cocktail_id를 타서 cocktail에 가서 칵테일 정보 가져오기
-        for (CocktailLists cl : cocktailLists) {
-            if ( users.getId().equals( cl.getUsers().getId() ) ) {
-                for (CocktailsDto c : cocktailsDtos) {
-                    if (cl.getCocktails().getId().equals(c.getId())) {
-                        ct.add( c );
-                    }
-                }
-            }
-        }
-
-        return ct;
-    }
-
-
-
-
-
-    // 회원 정보 수정 페이지 / 이름, 이메일, 생년월일(나중에), 전화번호(나중에) form에 넣어놓기
-    @PostMapping("/login/modify")
-    public UserRegisterDto userModify(HttpSession session) {
-        // 세션에서 "semail"이라는 키로 저장된 객체를 반환
-        // 출력: 유저 이메일
-        String email = (String) session.getAttribute("semail");
-        Users users = loginService.findByEmail( email );
-
-        UserRegisterDto to = new UserRegisterDto();
-        to.setId( users.getId() );
-        to.setEmail( users.getEmail() );
-        to.setName( users.getName() );
-
-        return to;
-    }
-
-    // 이메일은 변경 X
-    // 이메일로 유저의 Id를 가져와 비밀번호와 이름을 수정
-    @PostMapping("/login/modify_ok")
-    public int modify_ok(
-        @RequestParam("email") String email,
-        @RequestParam("name") String name,
-        @RequestParam("password") String password
-    ) {
-        int flag = 0;
-
-        Users users = loginService.findByEmail( email );
-
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String encodedPassword = encoder.encode(password);
-
-        flag = loginService.updateUser( name, encodedPassword, users.getId());
-
-
-        return flag;
-    }
-
-
-    @PostMapping("/withdrawal_ok")
-    public int userWithdrawal(
-        @RequestParam("email") String email,
-        @RequestParam("password") String password
-    ) {
-        int flag = 0;
-
-        Users users = loginService.selectByEmailandPassword( email, password );
-        // System.out.println("users.getEmail(): " + users.getEmail()); // 잘 나옴
-
-        int result = 0;
-        if(email.equals(users.getEmail()) && password.equals(users.getPassword())) {
-            result = loginService.deleteUser( users.getId() );
-        }
-
-        if ( result == 0 ) {
-            flag = 1;
-        } else {
-            flag = 0;
-        }
-
-        return flag;
-    }
 
 }
 
