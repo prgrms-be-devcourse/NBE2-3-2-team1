4cc8f6c13275373d6a426f454d7ba3f8c39580e3
JinWoong <slsl03264@naver.com>
2025-01-11 15:32:52 +0900

    fix:RestController로 변경해 Swagger로 보일 수 있도록 변경

--- src/main/java/org/programmers/cocktail/admin/controller/DashboardWebController.java
+++ src/main/java/org/programmers/cocktail/admin/controller/DashboardWebController.java
@@ -13,10 +13,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
-@Controller
+@RestController
 @RequestMapping("/admin")
 public class DashboardWebController {
 
